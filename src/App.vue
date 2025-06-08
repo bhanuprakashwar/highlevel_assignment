5d3e81c1c4b1dc96b870a502dc5a5219f85f0ce9
BhanuPrakash Bakkolla <36851105+bhanuprakashwar@users.noreply.github.com>
2025-06-09 00:00:44 +0530

    feat(vite): project setup with spacing components
    * feat(vite): project setup
    
    * feat(skeleton): created spacing components

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header></header>
+
+  <main class="mainClass">
+    <SpacingComponent />
+  </main>
+</template>
+
+<script setup lang="ts">
+import SpacingComponent from './components/SpacingComponent.vue'
+</script>
+
+<style scoped>
+.mainClass {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
