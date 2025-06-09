13ae7de42d11a5da5094b37dbd449c9c3fd3d9d4
BhanuPrakash Bakkolla <36851105+bhanuprakashwar@users.noreply.github.com>
2025-06-10 03:37:28 +0530

    feat(deployment): included gh-pages changes (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/bhanuprakash-high-level/',
   plugins: [
     vue(),
     vueDevTools(),
